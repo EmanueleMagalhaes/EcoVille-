fef5fad5bcc90d2600d02d60ce25e567d137c11a
Emanuele Magalhães <130506951+EmanueleMagalhaes@users.noreply.github.com>
2025-09-29 21:13:44 -0300

    Início do projeto EcoVille

---
+++ src/main/java/com/ecoville/EcovilleApplication.java
@@ -0,0 +1,13 @@
+package com.ecoville;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcovilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcovilleApplication.class, args);
+	}
+
+}
