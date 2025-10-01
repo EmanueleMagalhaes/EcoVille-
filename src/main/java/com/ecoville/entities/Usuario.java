a70f440f26572c113aa0e4efafad0714eb521264
HallanBrandt <112650184+hjuliano2004@users.noreply.github.com>
2025-10-01 19:47:39 -0300

    usuario, endereco e repositorios criados

---
+++ src/main/java/com/ecoville/entities/Usuario.java
@@ -0,0 +1,49 @@
+package com.ecoville.entities;
+
+import com.ecoville.enums.Perfil;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "nome_usuario", length = 50, unique = true)
+    private String NomeUsuario;
+
+    @Column(nullable = false, length = 64)
+    private String email;
+
+    @Column(nullable =  false, length = 6)
+    private String senha;
+
+    @Column(nullable = false)
+    private Perfil perfil;
+
+    @Column(nullable = false)
+    @OneToOne
+    private Endereco endereco;
+}
