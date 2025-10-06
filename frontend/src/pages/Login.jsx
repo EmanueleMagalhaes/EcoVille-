b7236ea63104dbfdddee07a79069201b5ff24b70
paulacalderaro <paula.dcalderaro@gmail.com>
2025-10-06 15:45:56 -0300

    Criando tela de cadastro e estilizando em css

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Tela de Login</h1>
+      <p>Em desenvolvimento...</p>
+    </div>
+  );
+}
+
+export default Login;
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,16 +1,16 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import CriarConta from "../pages/CriarConta";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/criar-conta" element={<CriarConta />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default AppRoutes
+export default AppRoutes;
