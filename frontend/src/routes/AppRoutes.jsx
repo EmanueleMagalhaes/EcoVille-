f549e277a8ba08ee17b14116df5a002298fcdbc9
Emanuele Magalhães <130506951+EmanueleMagalhaes@users.noreply.github.com>
2025-10-06 17:01:39 -0300

    fiz: ajuste de rota no arquivo AppRoutes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,13 +1,15 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login';
+
+import MinhaSolicitacoes from '../pages/MinhasSolicitacoes';
 
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        
+        <Route path="/solicitacoes" element={<MinhaSolicitacoes />} />
       </Routes>
     </BrowserRouter>
   );
