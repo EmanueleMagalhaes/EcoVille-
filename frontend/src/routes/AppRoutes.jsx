fd99357de794ef272e3b84dec7e7e20471a14134
HallanBrandt <112650184+hjuliano2004@users.noreply.github.com>
2025-10-07 16:07:20 -0300

    rota de login adicinada

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,13 +2,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import MinhaSolicitacoes from '../pages/MinhasSolicitacoes';
+import Login from '../pages/Login';
 
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route path="/" element={<Login />} />
         <Route path="/solicitacoes" element={<MinhaSolicitacoes />} />
       </Routes>
     </BrowserRouter>
