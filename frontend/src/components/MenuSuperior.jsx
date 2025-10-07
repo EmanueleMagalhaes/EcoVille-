7009e7492800676547e6576740acd647c2045e1b
paulacalderaro <paula.dcalderaro@gmail.com>
2025-10-06 21:30:34 -0300

    Adiciona tela de login e componente MenuSuperior

---
+++ frontend/src/components/MenuSuperior.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MenuSuperior() {
+  return (
+    <header style={{ 
+      backgroundColor: "#7B2CBF", 
+      color: "white", 
+      padding: "15px", 
+      textAlign: "center", 
+      fontWeight: "bold" 
+    }}>
+      EcoVille 🌱
+    </header>
+  );
+}
+
+export default MenuSuperior;
